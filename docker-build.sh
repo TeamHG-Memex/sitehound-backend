4b08407a8883c91bb72d58d93f4c29d2775ce718
Tomas <fornarat@gmail.com>
2017-08-28 16:22:46 +0200

    bumping docker version

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
-sitehound_backend_version=5.0.18
+sitehound_backend_version=5.1.0
 docker build --tag hyperiongray/sitehound-backend:$sitehound_backend_version .
 docker push hyperiongray/sitehound-backend:$sitehound_backend_version
 
