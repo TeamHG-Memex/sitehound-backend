f1247d97dec6cad6e9b4c1e1c5ae09ee0b3b8869
Tomas <fornarat@gmail.com>
2017-09-18 00:44:21 +0200

    bumping docker version

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,6 @@
-sitehound_backend_version=5.2.0
+sitehound_backend_version=5.2.3
 docker build --tag hyperiongray/sitehound-backend:$sitehound_backend_version .
-docker push hyperiongray/sitehound-backend:$sitehound_backend_version
+#docker push hyperiongray/sitehound-backend:$sitehound_backend_version
 
 docker run -d --name=sitehound-backend-$sitehound_backend_version --hostname=sitehound-backend -v /home/ubuntu/sitehound-backend/config/properties-override:/root/sitehound-backend/config/properties-override --link kafka-2.11-0.10.1.1-2.4:hh-kafka --link mongodb:mongodb --link elasticsearch:hh-elasticsearch hyperiongray/sitehound-backend:$sitehound_backend_version
 
